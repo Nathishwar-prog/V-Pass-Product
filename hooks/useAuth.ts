7286b88273f42e4b321a712411539cc2d6c1c5c1
Nathishwar <nathishwarc@gmail.com>
2026-01-14 13:04:24 +0530

    Add console log in useAuth for debugging

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -6,6 +6,7 @@ export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
+    console.log("This is new ");
   }
   return context;
 };
